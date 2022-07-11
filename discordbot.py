efeace6e406b79ea5bb27e1013b70de9f55d6ea4
degenwings <108446154+degenwings@users.noreply.github.com>
2022-07-10 23:05:05 -0400

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -42,13 +42,13 @@ async def on_message(message):
             image_url = message.attachments[0].url
         else:
             image_url = ""
-        await send_message(build_embed(author_name, message.author.avatar_url_as(format=None, static_format='png', size=1024), message.clean_content, message.author.color, image_url))
+        await send_message('***{0.author}***\n'.format(message) + message.content)
 
 
 @target_client.event
 async def send_message(message_embed):
     channel = target_client.get_channel(target_channel_id)
-    await channel.send(embed=message_embed)
+    await channel.send(message_embed)
 
 
 loop = asyncio.get_event_loop()
